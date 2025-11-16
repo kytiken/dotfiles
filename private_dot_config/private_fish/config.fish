3fa0cc98ab16eadf407c80b16bd81949684e6464
kytiken <kytiken@gmail.com>
2025-11-16 12:40:48 +0900

    fix: remove unnecessary blank line in fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
 ~/.local/bin/mise activate fish | source
 set -x EDITOR /opt/homebrew/bin/hx
 starship init fish | source
