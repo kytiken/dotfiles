0c2289bbe4ff9f74bab5b4b61fbdf0b26cbe8c0f
kytiken <kytiken@gmail.com>
2025-11-23 13:31:16 +0900

    feat: add comment and sort

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,8 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-~/.local/bin/mise activate fish | source
-set -x EDITOR /opt/homebrew/bin/hx
-starship init fish | source
+# abbreviations
 abbr zj zellij
+
+# setting path
 fish_add_path --path $HOME/.antigravity/antigravity/bin
+
+# set environment variables
+set -x EDITOR /opt/homebrew/bin/hx
+
+# setup tools
+~/.local/bin/mise activate fish | source
+starship init fish | source
