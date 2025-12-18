194a085b8d6666f889d2a6bae3f2039a4a0398e9
yamaguchi.kenta <yamaguchi.kenta@a-tm.co.jp>
2025-12-18 12:29:43 +0900

    config: add Rancher Desktop path management to fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -15,3 +15,7 @@ set -x EDITOR /opt/homebrew/bin/hx
 eval "$(/opt/homebrew/bin/brew shellenv)"
 ~/.local/bin/mise activate fish | source
 starship init fish | source
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "~/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
